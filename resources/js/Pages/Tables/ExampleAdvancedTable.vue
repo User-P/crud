682f75bbaf7904946950fb0e9d0bc797f1cd6c0f
User-P <pether.1994@gmail.com>
2026-02-03 12:25:22 -0600

    v10

--- resources/js/Pages/Tables/ExampleAdvancedTable.vue
+++ resources/js/Pages/Tables/ExampleAdvancedTable.vue
@@ -7,12 +7,17 @@
                 <Button type="button" class="p-button-sm p-button-info" label="Log Seleccionados"
                     @click="logSelected" />
             </div>
+            <div class="w-full sm:w-auto">
+                <label for="advanced-global-search" class="sr-only">Buscar</label>
+                <InputText id="advanced-global-search" v-model="globalSearch" type="search"
+                    class="w-full min-w-[12rem] sm:w-52 lg:w-64 text-sm p-2" placeholder="Buscar..." />
+            </div>
         </div>
 
         <div v-if="tableApi" class="rounded-lg border border-gray-200/80 bg-white p-4 shadow-sm">
             <div class="flex flex-col gap-3">
                 <div class="flex flex-wrap items-center justify-between gap-2">
-                    <div class="text-sm font-medium text-gray-700">Filtros y búsqueda</div>
+                    <div class="text-sm font-medium text-gray-700">Filtros</div>
                     <div class="flex items-center gap-3 text-xs text-gray-500">
                         <span>Filtrados: {{ filteredTotal }}</span>
                         <span v-if="totalRows !== filteredTotal">Total: {{ totalRows }}</span>
@@ -31,11 +36,6 @@
                         @click="filtersOpen = !filtersOpen" />
                 </div>
             </div>
-        </div>
-
-        <div class="flex justify-end mb-2">
-            <InputText v-model="globalSearch" type="search" class="w-full min-w-[12rem] sm:w-52 lg:w-64 text-sm p-2"
-                placeholder="Buscar..." />
         </div>
 
         <TanStackTable ref="tableRef" :data="data" :columns="columns" :loading="loading" enable-sorting
