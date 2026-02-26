8acbbe4face86d075cdf4701a08fec223373c4a9
User-P <pether.1994@gmail.com>
2026-02-26 17:40:34 -0600

    v0

--- app/Exports/ActionableExport.php
+++ app/Exports/ActionableExport.php
@@ -2,15 +2,33 @@
 
 namespace App\Exports;
 
-use Maatwebsite\Excel\Concerns\FromCollection;
+use App\Models\CatDirectivo;
+use App\Models\CatTiposAccionables;
+use App\Models\TblResultadoAccionable;
+use Maatwebsite\Excel\Concerns\ShouldAutoSize;
+use Maatwebsite\Excel\Concerns\WithMultipleSheets;
 
-class ActionableExport implements FromCollection
+class ActionableExport implements WithMultipleSheets, ShouldAutoSize
 {
-    /**
-    * @return \Illuminate\Support\Collection
-    */
-    public function collection()
+    protected $start_date;
+    protected $end_date;
+
+    public function __construct(string $start_date, string $end_date)
     {
-        
+        $this->start_date = $start_date;
+        $this->end_date = $end_date;
+    }
+
+    public function sheets(): array
+    {
+        $sheets = [];
+        $directivos = CatDirectivo::select('nm_directivo', 'siglas_directivo')->get();
+        $types = CatTiposAccionables::select('uuid_tipo_accionable', 'tipo_accionable')->get()->toArray();
+
+        foreach ($types as $type) {
+
+            $sheets[] = new CollaborationSheet($type, $this->start_date, $this->end_date, $directivos);
+        }
+        return $sheets;
     }
 }
